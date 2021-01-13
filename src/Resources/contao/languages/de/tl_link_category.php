bfdf6c5fd06afdd5b6544b98f546876144217764
Delirius - Daniel Herren <github@delirius.ch>
2021-01-13 16:52:27 +0100

    typo in template

--- src/Resources/contao/languages/de/tl_link_category.php
+++ src/Resources/contao/languages/de/tl_link_category.php
@@ -3,7 +3,7 @@
 /**
  * Fields
  */
- $GLOBALS['TL_LANG']['tl_link_category']['title'] = array('Titel intern', '(Footer 01, Footer 02, oä.) Titel intern wird verwendet für die Darstellung und Reihenfolge im Backend');
+ $GLOBALS['TL_LANG']['tl_link_category']['title'] = array('Titel intern', 'Titel intern wird verwendet für die Darstellung und Reihenfolge im Backend (Footer 01, Footer 02, oä.).');
  $GLOBALS['TL_LANG']['tl_link_category']['title_publik'] = array('Titel', '');
 $GLOBALS['TL_LANG']['tl_link_category']['published'] = array('Publiziert', '');
 $GLOBALS['TL_LANG']['tl_link_category']['description'] = array('Beschreibung', '');
